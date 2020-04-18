53bbaadd4f05bf6db1c4d960d70442862ff7e4fc
susanahmadi <info@suamd.ir>
2020-04-18 21:54:26 +0430

    editting6

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,9 +2,9 @@
         <footer class="dorne-footer-area">
             <div class="container-fluid section-padding-50-50">
                 <div class="row ">
-                    <div class="col-sm-3 align-items-right justify-content-between ">
+                    <div class="col-sm-6 align-items-right justify-content-between ">
                         <div class="section-heading dark text-center">
-                            <a href="/aboutus"><h5> درباره ما </h5></a>
+                           <h5> درباره ما </h5>
                             <span></span>
                             <p>
                             وبسایت بیوتی شو، وبسایت ثبت آگهی در زمینه مراکز آرایشی و زیبایی میباشد .هدف از ایجاد این وبسایت نمایش آگهی های مختص به مراکز آرایشی ، پزشکان زیبایی ، مراکز زیبایی دندان ، مراکز زیبایی پوست ، مراکز زیبایی مو ، مراکز مخصوص تناسب اندام  و... میباشد .این وبسایت از پذیرش سایر آگهی ها معذور است .کلیه مراکز آرایشی و زیبایی میتوانند بسته به نوع آگهی مد نظر خود اطلاعات خود را به همراه تصاویر مربوطه در سایت قرار دهند .لذا خواهشمند است در ارسال اطلاعات و تصاویر دقت کافی را مبذول فرمایید .کیفیت تصاویر باید قابل قبول باشد و تصاویر بی کیفیت حذف خواهند شد .این وبسایت هیچ مسئولیتی نسبت به صحت اطلاعات مندرج در آگهی ها ندارد و بازدید کنندگان گرامی باید دقت کافی را در این زمینه داشته باشند .
@@ -13,30 +13,31 @@
                     </div>
                     <div class="col-sm-3 align-items-right justify-content-between">
                         <div class="section-heading dark text-center">
-                            <a href="/contact/create"><h5> تماس با ما </h5></a>
+                            <h5> تماس با ما </h5>
                             <span></span>
                             <p>
-                            <i class="fa fa-envelope"></i> پشتیبانی: suport@beautyshoo.ir <br/>
-                            <i class="fa fa-envelope"></i> ایمیل : info@beautyshoo.ir <br/>
-                            <i class="fa fa-phone"></i> تبلیغات: 09125704008 <br/>
-                            <i class="fa fa-phone"></i> دفتر مرکزی: 02188361055 <br/>
+                            <i class="fa fa-envelope"></i> پشتیبانی: suport@beautyshoo.ir <br/><br/>
+                            <i class="fa fa-envelope"></i> ایمیل : info@beautyshoo.ir <br/><br/>
+                            <i class="fa fa-phone"></i> تبلیغات: 09125704008 <br/><br/>
+                            <i class="fa fa-phone"></i> دفتر مرکزی: 02188361055 <br/><br/>
                             <i class="fa fa-map-marker"></i> آدرس: تهران - سعادت آباد میدان کاج - سرو غربی <br/>
                             </p>
                         </div>
                     </div>
                     <div class="col-sm-3 align-items-right justify-content-between">
                         <div class="section-heading dark text-center">
-                            <a href="/cat/last"><h5> آخرین آگهی ها </h5></a>
+                            <h5> با بیوتی شو همراه شوید </h5>
                             <span></span>
-                           
+                           <ul class="footer-links">
+                               <li> <a href=""> سالن های زیبایی</a></li>
+                               <li> <a href=""> آموزشگاه های زیبایی</a></li>
+                               <li> <a href=""> خدمات زیبایی</a></li>
+                               <li> <a href=""> لوازم آرایشی و بهداشتی</a></li>
+                            
+                           </ul>
                         </div>
                     </div>
-                    <div class="col-sm-3 align-items-right justify-content-between">
-                        <div class="section-heading dark text-center">
-                            <a href="/blog"><h5> آخرین مطالب </h5></a>
-                            <span></span>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
                 <div class="container-fluid border-top">
