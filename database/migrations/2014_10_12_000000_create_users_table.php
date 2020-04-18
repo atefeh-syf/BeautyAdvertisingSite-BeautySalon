0644e9b6e347e104e0aa793592f23d4dcc68890a
atefeh-syf <atefeh.syf@gmail.com>
2020-04-18 20:06:41 +0430

    end

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,13 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('image')->unique();
+            $table->string('image')->nullable();
             $table->string('username')->unique()->nullable();
-            $table->string('phone')->unique()->nullable();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            
             $table->timestamps();
         });
     }
